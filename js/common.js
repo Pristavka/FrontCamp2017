36eaf549ee7f1dcf6d452760949346b4ab09d4e8
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2017-11-27 17:32:49 +0300

    fixed comments

--- js/common.js
+++ js/common.js
@@ -1,6 +1,8 @@
 ; (() => {
-  setCurrentDate = () => {
-    let date = new Date();
+  const api_key = 'eefc0a2a311a42dc8936f6dceba17753';
+
+  const setCurrentDate = () => {
+    const date = new Date();
     const options = {
       year: 'numeric',
       month: 'long',
@@ -9,19 +11,18 @@
     };
     document.querySelector('.header__date').innerHTML = date.toLocaleString("en-US", options)
   }
-  setCurrentDate();
 
   // Common getNews method
-  getNews = (url) => {
+  const getNews = url => {
     return fetch(url)
-      .then((resp) => resp.json())
+      .then(resp => resp.json())
   }
 
   // Functionality for fetching and rendering Latest News
-  getLatestNews = () => {
-    getNews('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=eefc0a2a311a42dc8936f6dceba17753')
-      .then((latestNews) => {
-        let news = `
+  const getLatestNews = () => {
+    getNews(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${api_key}`)
+      .then(latestNews => {
+        const news = `
           <h3 class="latest__source">${latestNews.articles[0].author}</h3>
           <div class="latest__img">
             <a href="${latestNews.articles[0].url}" target="_blank">
@@ -36,16 +37,15 @@
         document.querySelector('.latest__itemNews').innerHTML = news;
       })
   }
-  getLatestNews();
 
   // Functionality for fetching and rendering Sport News
   const listOfsportNews = document.querySelector('.listOfsportNews');
-  renderSportNews = (allnews) => {
+  const renderSportNews = allnews => {
     listOfsportNews.innerHTML = '';
-    allnews.articles.forEach((art) => {
-      let div = document.createElement('div');
+    allnews.articles.forEach(art => {
+      const div = document.createElement('div');
       div.className = 'sport__item';
-      let news = `
+      const news = `
         <p class="latest__title">${art.title}</p>
         <p class="latest__shortDescr">${art.description}</p>
         <p class="latest__url"><a href="${art.url}" target="_blank">Open original source</a></p>
@@ -55,20 +55,24 @@
     })
   }
 
-  getESPNNews = () => {
-    getNews('https://newsapi.org/v2/top-headlines?sources=espn&apiKey=eefc0a2a311a42dc8936f6dceba17753')
-      .then((allnews) => renderSportNews(allnews));
+  const getESPNNews = () => {
+    getNews(`https://newsapi.org/v2/top-headlines?sources=espn&apiKey=${api_key}`)
+      .then(allnews => renderSportNews(allnews));
   }
-  getNHLNews = () => {
-    getNews('https://newsapi.org/v2/top-headlines?sources=nhl-news&apiKey=eefc0a2a311a42dc8936f6dceba17753')
-      .then((allnews) => renderSportNews(allnews));
+  const getNHLNews = () => {
+    getNews(`https://newsapi.org/v2/top-headlines?sources=nhl-news&apiKey=${api_key}`)
+      .then(allnews => renderSportNews(allnews));
   }
-  getMarcaNews = () => {
-    getNews('https://newsapi.org/v2/top-headlines?sources=marca&apiKey=eefc0a2a311a42dc8936f6dceba17753')
-      .then((allnews) => renderSportNews(allnews));
+  const getMarcaNews = () => {
+    getNews(`https://newsapi.org/v2/top-headlines?sources=marca&apiKey=${api_key}`)
+      .then(allnews => renderSportNews(allnews));
   }
 
   document.querySelector('.espn').addEventListener('click', getESPNNews);
   document.querySelector('.nhl').addEventListener('click', getNHLNews);
   document.querySelector('.marca').addEventListener('click', getMarcaNews);
+
+  setCurrentDate();
+  getLatestNews();
+
 })()
