9991ff0d218116d12814a587bd10e3f18a19b267
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2017-11-27 18:40:04 +0300

    changed structure of common js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,11 @@
 ; (() => {
   const api_key = 'eefc0a2a311a42dc8936f6dceba17753';
+  const resources = {
+    bbc: `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${api_key}`,
+    espn: `https://newsapi.org/v2/top-headlines?sources=espn&apiKey=${api_key}`,
+    nhl: `https://newsapi.org/v2/top-headlines?sources=nhl-news&apiKey=${api_key}`,
+    marca: `https://newsapi.org/v2/top-headlines?sources=marca&apiKey=${api_key}`
+  }
 
   const setCurrentDate = () => {
     const date = new Date();
@@ -12,33 +18,24 @@
     document.querySelector('.header__date').innerHTML = date.toLocaleString("en-US", options)
   }
 
-  // Common getNews method
-  const getNews = url => {
-    return fetch(url)
-      .then(resp => resp.json())
+  // Render Latest News
+  const renderLatestNews = latestNews => {
+    const news = `
+    <h3 class="latest__source">${latestNews.articles[0].author}</h3>
+    <div class="latest__img">
+      <a href="${latestNews.articles[0].url}" target="_blank">
+        <img src="${latestNews.articles[0].urlToImage}" alt="${latestNews.articles[0].title}">
+      </a>
+    </div>
+    <p class="latest__title">${latestNews.articles[0].title}</p>
+    <p class="latest__shortDescr">${latestNews.articles[0].description}</p>
+    <p class="latest__url"><a href="${latestNews.articles[0].url}" target="_blank">Open original source</a></p>
+    <p class="latest__published"><b>Date of publication:</b> ${latestNews.articles[0].publishedAt}</p>
+  `
+  document.querySelector('.latest__itemNews').innerHTML = news;
   }
 
-  // Functionality for fetching and rendering Latest News
-  const getLatestNews = () => {
-    getNews(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${api_key}`)
-      .then(latestNews => {
-        const news = `
-          <h3 class="latest__source">${latestNews.articles[0].author}</h3>
-          <div class="latest__img">
-            <a href="${latestNews.articles[0].url}" target="_blank">
-              <img src="${latestNews.articles[0].urlToImage}" alt="${latestNews.articles[0].title}">
-            </a>
-          </div>
-          <p class="latest__title">${latestNews.articles[0].title}</p>
-          <p class="latest__shortDescr">${latestNews.articles[0].description}</p>
-          <p class="latest__url"><a href="${latestNews.articles[0].url}" target="_blank">Open original source</a></p>
-          <p class="latest__published"><b>Date of publication:</b> ${latestNews.articles[0].publishedAt}</p>
-        `
-        document.querySelector('.latest__itemNews').innerHTML = news;
-      })
-  }
-
-  // Functionality for fetching and rendering Sport News
+  // Render Sport News
   const listOfsportNews = document.querySelector('.listOfsportNews');
   const renderSportNews = allnews => {
     listOfsportNews.innerHTML = '';
@@ -55,24 +52,26 @@
     })
   }
 
-  const getESPNNews = () => {
-    getNews(`https://newsapi.org/v2/top-headlines?sources=espn&apiKey=${api_key}`)
-      .then(allnews => renderSportNews(allnews));
+  // Common getNews method
+  const getNews = url => {
+    return fetch(url)
+      .then(resp => resp.json())
   }
-  const getNHLNews = () => {
-    getNews(`https://newsapi.org/v2/top-headlines?sources=nhl-news&apiKey=${api_key}`)
-      .then(allnews => renderSportNews(allnews));
+
+  const getLatestNews = () => {
+    getNews(resources.bbc)
+      .then( latestNews => renderLatestNews(latestNews))
   }
-  const getMarcaNews = () => {
-    getNews(`https://newsapi.org/v2/top-headlines?sources=marca&apiKey=${api_key}`)
+
+  const getSportNews = (url) => {
+    getNews(url)
       .then(allnews => renderSportNews(allnews));
   }
 
-  document.querySelector('.espn').addEventListener('click', getESPNNews);
-  document.querySelector('.nhl').addEventListener('click', getNHLNews);
-  document.querySelector('.marca').addEventListener('click', getMarcaNews);
+  document.querySelector('.espn').addEventListener('click', () => {getSportNews(resources.espn)});
+  document.querySelector('.nhl').addEventListener('click', () => {getSportNews(resources.nhl)});
+  document.querySelector('.marca').addEventListener('click', () => {getSportNews(resources.marca)});
 
   setCurrentDate();
   getLatestNews();
-
 })()
