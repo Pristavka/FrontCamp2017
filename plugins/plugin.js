e3f640ee8f4d80ff556751bd218eb6b9d4ea40ad
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2017-12-05 12:06:12 +0300

    fixed codestyle

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -1,11 +1,16 @@
-module.exports = function ({types: t}) { 
+module.exports = function ({ types: t }) {
   return {
     visitor: {
       BindExpression(path) {
         const newNode = t.memberExpression(
-          t.memberExpression(t.memberExpression(t.identifier(path.node.object.name), t.identifier('prototype')),t.identifier(path.node.callee.name)), t.identifier('call')
-            );
-        path.replaceWith(newNode);  
+          t.memberExpression(
+            t.memberExpression(
+              t.identifier(path.node.object.name),
+              t.identifier('prototype')),
+            t.identifier(path.node.callee.name)),
+          t.identifier('call')
+        );
+        path.replaceWith(newNode);
       }
     }
   };
