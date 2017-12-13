12a718eca88cb8acff0f09907323f836d1310b9c
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2017-12-13 11:17:51 +0300

    added es6 to loader

--- loaders/custom-loader.js
+++ loaders/custom-loader.js
@@ -1,9 +1,8 @@
-module.exports = function (source) {
-  var jsn = JSON.parse(source);
-  var newJSON = {};
-  for(var index in jsn){
+module.exports = source => {
+  const jsn = JSON.parse(source);
+  let newJSON = {};
+  for(let index in jsn){
     if (!Number(index)) newJSON[index] = jsn[index];
   }
-
   return JSON.stringify(newJSON, null, '\t');
 }
