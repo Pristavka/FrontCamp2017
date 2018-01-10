2c5a0afd17b940f2453d1e9556024c1bbf90fd5d
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2018-01-10 12:54:48 +0300

    remove unused var

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import 'babel-polyfill';
 import MainComponent from './components/mainComponent';
-import { configs } from './config/config';
 // eslint-disable-next-line no-unused-vars
 import * as staticFiles from './components/staticFilesComponent';
 
