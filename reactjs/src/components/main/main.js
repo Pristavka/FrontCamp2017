bd64af32db9a3f6124660062b0e2009836444f3b
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2018-02-19 14:56:53 +0300

    added addPost component, added success msg

--- reactjs/src/components/main/main.js
+++ reactjs/src/components/main/main.js
@@ -6,17 +6,22 @@ import Footer from '../footer/footer';
 import AddPost from '../addPost/addPost';
 import PostList from '../postsList/postsList';
 
+import { config } from '../../config/config';
 import { mockPosts } from '../../mockdata/mockPosts';
 
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showComponent: 'show all posts',
-      posts: mockPosts
+      showComponent: config.pages.postsList,
+      posts: mockPosts,
+      showSuccess: false
     };
     this.showComponent = this.showComponent.bind(this);
     this.filterPosts = this.filterPosts.bind(this);
+    this.addPosts = this.addPosts.bind(this);
+    this.showMessage = this.showMessage.bind(this);
+    this.renderMessage = this.renderMessage.bind(this);
   }
 
   showComponent(component) {
@@ -27,16 +32,43 @@ export default class Main extends React.Component {
     this.setState({ posts });
   }
 
+  addPosts(post) {
+    let posts = this.state.posts;
+    posts.push(post);
+    this.setState({ posts });
+  }
+
+  showMessage() {
+    this.setState({ showSuccess: true});
+    setTimeout(() => this.setState({ showSuccess: false }), 5000);
+  }
+
+  renderMessage() {
+    return (
+      <div className={styles.successMsg}>
+          {config.messages.success}
+      </div>
+    )
+  }
+
   render() {
     return (
       <React.Fragment>
         <Header showComponent={this.showComponent}/>
+        {this.state.showSuccess ? this.renderMessage() : null}
         <div className={styles.wrapper}>
           <div className={styles.content}>
             {(() => {
               switch (this.state.showComponent) {
-              case 'show all posts': return <PostList posts={this.state.posts} filterPosts={this.filterPosts}/>;
-              case 'add post': return <AddPost />;
+              case config.pages.postsList: return <PostList
+                posts={this.state.posts}
+                filterPosts={this.filterPosts}
+              />;
+              case config.pages.addPost: return <AddPost
+                addPosts={this.addPosts}
+                showComponent={this.showComponent}
+                showMessage={this.showMessage}
+              />;
             }})()}
           </div>
         </div>
