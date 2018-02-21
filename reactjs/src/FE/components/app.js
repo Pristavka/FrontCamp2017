d9c009e64679915801de56bf4c1c7012553eb43f
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2018-02-21 12:16:52 +0300

    some fix

--- reactjs/src/FE/components/app.js
+++ reactjs/src/FE/components/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Main from './main/main';
-import './assets/global.scss';
+import '../assets/global.scss';
 
 export default class App extends React.Component {
   render() {
