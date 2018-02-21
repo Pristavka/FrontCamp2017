c4957ff28458c2b21eca7aba87394f30aef145f9
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2018-02-21 12:49:53 +0300

    fixed webpack config and added plugin for babel

--- reactjs/src/BE/renderTemplate.js
+++ reactjs/src/BE/renderTemplate.js
@@ -10,6 +10,7 @@ const renderPage = (html) => {
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="css/style.bundle.css">
       <title>Welcome, Friends!</title>
     </head>
     <body>
--- reactjs/webpack.config.js
+++ reactjs/webpack.config.js
@@ -4,20 +4,19 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
 const paths = {
-  DIST: path.resolve(__dirname, 'dist'),
+  PUBLIC: path.resolve(__dirname, 'public'),
   frontSRC: path.resolve(__dirname, 'src/FE'),
   backSRC: path.resolve(__dirname, 'src/BE')
 };
 
 const frontConfig = {
-  devtool: 'source-map',
   entry: path.join(paths.frontSRC, 'index.js'),
   output: {
-    path: paths.DIST,
-    filename: './public/[name].bundle.js'
+    path: paths.PUBLIC,
+    filename: './[name].bundle.js'
   },
   plugins: [
-    new ExtractTextPlugin('./public/style.bundle.css'),
+    new ExtractTextPlugin('./css/style.bundle.css'),
   ],
   module: {
     rules: [
@@ -25,7 +24,6 @@ const frontConfig = {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
-        query: { presets: ["env", "react", "stage-0"]}
       },
       {
         test: /\.s?css$/,
@@ -58,7 +56,7 @@ const backConfig = {
   entry: path.join(paths.backSRC, 'index.js'),
   target: 'node',
   output: {
-    path: paths.DIST,
+    path: paths.PUBLIC,
     filename: 'server.js',
     libraryTarget: 'commonjs'
   },
@@ -68,7 +66,6 @@ const backConfig = {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
-        query: { presets: ["env", "react", "stage-0"]}
       },
       {
         test: /\.s?css$/,
